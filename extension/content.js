fa185f10a4a4829e4af2b8ef89d0bbe498396218
dianabaumann <dibaberlin@icloud.com>
2022-07-27 15:04:08 +0200

    logic done

--- extension/content.js
+++ extension/content.js
@@ -1,15 +1,13 @@
 let tiltdb = chrome.runtime.getURL("tiltdb.json");
 let urlElements = document.getElementsByClassName("yuRUbf");
 
-const labels = [
-    [chrome.runtime.getURL("images/not_found.png"), "none"],
-    [chrome.runtime.getURL("images/green_icon_128.png"), "green"],
-    [chrome.runtime.getURL("images/yellow_icon_128.png"), "yellow"],
-    [chrome.runtime.getURL("images/red_icon_128.png"), "red"],
+const labels = [chrome.runtime.getURL("images/not_found.png"),
+chrome.runtime.getURL("images/green_icon_32.png"),
+chrome.runtime.getURL("images/yellow_icon_32.png"),
+chrome.runtime.getURL("images/red_icon_32.png")
 ];
 
 let tiltDoc = {
-    score: "0",
     "Data Disclosed": "",
     "Third Country Transfers": "",
     "Right to Withdraw Consent": "",
@@ -21,7 +19,7 @@ let tiltDoc = {
     "Automated Decision Making": "",
 };
 
-function getDomain(url) {
+async function getDomain(url) {
     url = url.split("/")[0];
     let url_split = url.split(".");
     if (url_split.length >= 3) {
@@ -31,71 +29,75 @@ function getDomain(url) {
     return url;
 }
 
-function printLabels() {
-    //add popup next to result
-    //let popup = $("<div class='list'> <div class='entry'> <img class='code-selector' alt='Label Image' src='" + labels[label][0] + "> <div class='content'> <div class='inner'> <h2>" + tracker + " Trackers</h2> <h4> From:</h4>" + logos_normal + "</div> </div> </div> </div > ");
-    //popup.appendTo(div);
-    //$("head").append("<link type=\"text/css\" rel=\"stylesheet\" href=\"style.css\">");
-}
+async function getScore(tiltHubEntry) {
+    let score = 0;
 
-function getScore(domain, tiltHubEntry) {
-    score = 1;
-    let tiltInfo = tiltHubEntry[1];
-
-    tiltDoc["Right to Complain"] = tiltInfo["rightToComplain"]["available"];
-    tiltDoc["Right to Withdraw Consent"] = tiltInfo["rightToWithdrawConsent"]["available"];
-    tiltDoc["Right to Data Portability"] = tiltInfo["rightToDataPortability"]["available"];
-    tiltDoc["Right to Information"] = tiltInfo["rightToInformation"]["available"];
-    tiltDoc["Right to Rectification or Deletion"] = tiltInfo["rightToRectificationOrDeletion"]["available"];
-    tiltDoc["Automated Decision Making"] = tiltInfo["automatedDecisionMaking"]["available"];
+    tiltDoc["Right to Complain"] = tiltHubEntry["rightToComplain"]["available"];
+    tiltDoc["Right to Withdraw Consent"] = tiltHubEntry["rightToWithdrawConsent"]["available"];
+    tiltDoc["Right to Data Portability"] = tiltHubEntry["rightToDataPortability"]["available"];
+    tiltDoc["Right to Information"] = tiltHubEntry["rightToInformation"]["available"];
+    tiltDoc["Right to Rectification or Deletion"] = tiltHubEntry["rightToRectificationOrDeletion"]["available"];
+    tiltDoc["Automated Decision Making"] = tiltHubEntry["automatedDecisionMaking"]["available"];
 
     Object.values(tiltDoc).forEach((value) => { if (!value) { score += 0.3; } });
 
-    tiltDoc["Data Protection Officer"] = tiltInfo["dataProtectionOfficer"]["email"];
-    if ((tiltInfo["dataProtectionOfficer"]["email"] = null)) { score += 0.3; }
+    tiltDoc["Data Protection Officer"] = tiltHubEntry["dataProtectionOfficer"]["email"];
+    if ((tiltHubEntry["dataProtectionOfficer"]["email"] = null)) { score += 0.3; }
 
-    tiltDoc["Third Country Transfers"] = tiltInfo["thirdCountryTransfers"].length;
-    if (tiltInfo["thirdCountryTransfers"].length > 1) { score += 0.3; }
+    tiltDoc["Third Country Transfers"] = tiltHubEntry["thirdCountryTransfers"].length;
+    if (tiltHubEntry["thirdCountryTransfers"].length > 1) { score += 0.3; }
 
-    tiltDoc["Data Disclosed"] = tiltInfo["dataDisclosed"];
-    if (tiltDoc["dataDisclosed"] != null) { score += 0.3; }
+    tiltDoc["Data Disclosed"] = tiltHubEntry["dataDisclosed"];
+    if (tiltHubEntry["dataDisclosed"] != null) { score += 0.3; }
 
-    tiltDoc["score"] = score;
-    chrome.storage.local.set({ [domain]: score });
-    console.log("New value was added to your local storage.")
     return score;
 }
 
-for (let element of urlElements) {
-    let url = JSON.stringify(element.children[0].href);
-    url = url.replace("https://", "");
-    let domain = getDomain(url);
-    let score = 0;
+async function getAllTiltScores() {
+    let tilt_all_scores = {};
+    //calculate tilt score for all tiltHub entries
+
+    await fetch(tiltdb).then((response) => response.json()).then((tiltHub) => {
+        tiltHub.forEach(async (tiltHubEntry) => {
+            let tilt_domain = await getDomain(tiltHubEntry["meta"]["url"].split("//")[1]);
+            score = await getScore(tiltHubEntry);
+            tilt_all_scores[tilt_domain] = score;
+        })
+    })
+    return (tilt_all_scores);
+}
+
+async function printLabels(score, heading) {
+    let result;
+
+    if (score == 0) { result = 0; } else { result = score < 1 ? 1 : score < 2 ? 2 : 3; }
+
+    var image = new Image();
+    image.src = labels[result];
+    heading.prepend(image);
+}
+
+async function main() {
+    let label = 0;
+    let tilt_all_scores = await getAllTiltScores();
+    let results = {};
     let index = 0;
 
-    fetch(tiltdb)
-        .then((response) => response.json())
-        .then((tiltHubEntry) => {
-            while (index < tiltHubEntry.length) {
-                let urlEntry = tiltHubEntry[index]["meta"]["url"].split("//")[1];
-                urlEntry = getDomain(urlEntry);
+    for (let e of urlElements) {
+        let url = JSON.stringify(e.children[0].href).replace("https://", "");
+        let domain = await getDomain(url);
+        results[domain] = 0;
 
-                if (urlEntry === domain) { // Is there an entry in the TILT database?
-                    //first, check the local storage, if the score was already calculated
-                    chrome.storage.local.get(function (data) {
-                        let domains = Object.keys(data);
-                        let scores = Object.values(data);
-
-                        domains.forEach((element, i) => {
-                            if (element == domain) {
-                                score = scores[i];
-                                console.log("The score was found in your local storage: " + domain + ", " + score);
-                            }
-                        });
-                    })
-                    //printLabels();
-                }
-                index++;
+        Object.keys(tilt_all_scores).forEach(async (key) => {
+            if (key == domain) {
+                results[domain] = tilt_all_scores[key];
             }
         });
+        console.log(results[domain]);
+        const headings = document.getElementsByClassName("LC20lb MBeuO DKV0Md");
+        label = await printLabels(results[domain], headings[index]);
+        index++;
+    }
 }
+
+main();
